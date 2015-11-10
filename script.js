7ffc4c1cb5dbe31c86e02aea0663786da7dd7541
adesanto <aarondesanto@gmail.com>
2015-11-10 15:41:11 -0500

    OK for real functionality is 100%

--- script.js
+++ script.js
@@ -8,28 +8,7 @@ $('#userSubmit').click(function(event) {
 	var itemPrice = $('#priceInput').val();
 	total += parseFloat(itemPrice);
 	$('ul').append("<li>" + itemName + ": $" + itemPrice + "</li>");
-	
 	$('#totalP').text("Total: $" + total);
-	console.log(itemName);
-	console.log(itemPrice);
-	console.log(total);
 });
-
-
-// $('li').hover(function() {
-	// $(this).finish();
-	// $(this).fadeTo(200, 0.25);
-// }, function() {
-	// $(this).finish();
-	// $(this).fadeTo(200, 1);
-// });
-
-// $('li').click(function() { 
-	// $(this).toggleClass('asdf');
-	// $(this).text($(this).text() === 'Clicked!' ? '' : 'Clicked!');
-// });
-
-
-
 
 });
